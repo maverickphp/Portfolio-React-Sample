bb808d9e3c87e12dd832d8123a438972cbaa3b54
maverickphp <fasih682@gmail.com>
2022-12-17 17:26:49 +0500

    Updated Image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from './components/UI/NavBar';
 import Card from './components/UI/Card';
 
+
 function App() {
   return (
     <Router>
 <NavBar/>
 <Card/>
+
       <Routes>
         <Route />
       </Routes>
